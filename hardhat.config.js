fdc296966d3b67bc489c4f2702fa70cf645a0718
mok <mok0230@protonmail.com>
2021-12-06 19:21:38 -0800

    successfully make calls to stEth contract on forked mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,43 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-module.exports = {
-  solidity: "0.8.4",
-  networks: {
+const networks = {};
+
+if(process.env.FORKING_URL) {
+  networks.hardhat = {
+    chainId: 31337,
     forking: {
-      url: `${process.env.FORKING_URL}`, // by default forks the latest block
-    } 
+      url: process.env.FORKING_URL,
+      blockNumber: 13756178
+    }
   }
+}
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    ...networks,
+  },
 };
+
+// console.log('process.env.FORKING_URL', process.env.FORKING_URL)
+//  const CHAIN_IDS = {
+//     hardhat: 31337, // chain ID for hardhat testing
+//   };
+  
+//   module.exports = {
+//     networks: {
+//       hardhat: {
+//         chainId: CHAIN_IDS.hardhat,
+//         forking: {
+//           // Using Alchemy
+//           url: `${process.env.FORKING_URL}`,
+//           // Using Infura
+//           // url: `https://mainnet.infura.io/v3/${INFURA_KEY}`, // ${INFURA_KEY} - must be your API key
+//           blockNumber: 12821000, // a specific block number with which you want to work
+//         },
+//       },
+//       // you can also add more necessary information to your config
+//     },
+//     solidity: "0.8.0"
+//   }
