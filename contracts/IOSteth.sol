ffe6723a0a37bfcb44d94a47a3fb9926777ef83c
alanckeegan <alan.c.keegan@gmail.com>
2021-12-07 11:25:41 -0500

    deleting comments form erc20s

--- contracts/IOSteth.sol
+++ contracts/IOSteth.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// NEED TO MODIFY TO GIVE MINT/BURN PRIVILEGES TO THE STRIP CONTRACT ON DEPLOYMENT
 contract IOSTeth is ERC20 {
     constructor(uint256 initialSupply) ERC20("IO-stETH-2023", "IOS") {
-      // NEED TO MODIFY TO GIVE MINT/BURN PRIVILEGES TO THE STRIP CONTRACT ON DEPLOYMENT
         _mint(msg.sender, initialSupply);
     }
 }
--- contracts/POSteth.sol
+++ contracts/POSteth.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract POSteth is ERC20 {
     constructor(uint256 initialSupply) ERC20("PO-stETH-2023", "POS") {
-      // NEED TO MODIFY TO GIVE MINT/BURN PRIVILEGES TO THE STRIP CONTRACT ON DEPLOYMENT
         _mint(msg.sender, initialSupply);
     }
 }
